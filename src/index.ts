dae8558e16b68da9cd4c79e219baa833fe43f610
sean <sean@gomicorp.com>
2022-03-21 01:09:39 +0900

    prisma instance 생성 후 context 로 전달

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
+import { Prisma, PrismaClient } from '@prisma/client';
 import { ApolloServer } from 'apollo-server';
 
 import { Query } from '@/resolvers';
 import { typeDefs } from '@/schema';
 
+export interface Context {
+  prisma: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation
+  >;
+}
+
+const prisma = new PrismaClient();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
     Query,
+  },
+  context: {
+    prisma,
   },
 });
 
