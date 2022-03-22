e571db03066b28de145194c73d144ff73bb295d3
sean <sean@gomicorp.com>
2022-03-22 23:03:00 +0900

    me 구현

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,8 +1,21 @@
-import { Post } from '@prisma/client';
+import { Post, User } from '@prisma/client';
 
 import { Context } from '@/index';
 
 export const Query = {
+  me: async (
+    _: any,
+    __: any,
+    { prisma, userInfo: { userId } }: Context,
+  ): Promise<User | null> => {
+    if (!userId) {
+      return null;
+    }
+    return prisma.user.findUnique({
+      where: { id: userId },
+    });
+  },
+
   posts: (_: any, __: any, { prisma }: Context): Promise<Post[]> => {
     return prisma.post.findMany({
       orderBy: [
