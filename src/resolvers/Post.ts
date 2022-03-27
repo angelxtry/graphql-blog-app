f5e3233d1c8884354004d30d610ed3285beef68d
sean <sean@gomicorp.com>
2022-03-27 22:06:07 +0900

    post -> author 코드 개선

--- src/resolvers/Post.ts
+++ src/resolvers/Post.ts
@@ -1,13 +1,9 @@
 import { Post as PostType } from '@prisma/client';
 
-import { Context } from '@/index';
 import { userLoader } from '@/loaders/user.loader';
 
 export const Post = {
-  author: ({ authorId }: PostType, __: any, { prisma }: Context) => {
-    // return prisma.user.findUnique({
-    //   where: { id: authorId },
-    // });
+  author: ({ authorId }: PostType) => {
     return userLoader.load(authorId);
   },
 };
